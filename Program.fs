4026bee38b457d1bffed814391fe174626e35273
Marc Timothy Huber <151627837+marctimothyhuber@users.noreply.github.com>
2024-12-07 08:45:07 +0100

    Upload Project

---
+++ Program.fs
@@ -0,0 +1,225 @@
+﻿open System
+open System.IO
+open System.Net.Http
+open Newtonsoft.Json.Linq
+
+type Account = { Type: string; Currency: string; Balance: decimal }
+
+let cashAccountCHF = { Type = "Cash"; Currency = "CHF"; Balance = 0M }
+let bankAccountCHF = { Type = "Bank"; Currency = "CHF"; Balance = 1000M }
+let bankAccountUSD = { Type = "Bank"; Currency = "USD"; Balance = 0M }
+let bankAccountEUR = { Type = "Bank"; Currency = "EUR"; Balance = 0M }
+
+// Fetch exchange rates from API
+let fetchExchangeRates apiKey =
+    async {
+        let url = sprintf "%s" apiKey
+        use client = new HttpClient()
+        let! response = client.GetStringAsync(url) |> Async.AwaitTask
+        return JObject.Parse(response)
+    }
+// Calculate Exchange Rate
+let calculateExchangeRate (fromCurrency: string) (toCurrency: string) (exchangeRates: JObject) =
+    let fromRate = exchangeRates.["data"].[fromCurrency].Value<decimal>()
+    let toRate = exchangeRates.["data"].[toCurrency].Value<decimal>()
+    toRate / fromRate
+
+let prepareResult (amount, clear) =
+    if clear then
+        Console.Clear()
+    for i in 1 .. amount do
+        printfn "--------------------------------"
+
+let withdraw amount (bankAccountCHF: Account) (cashAccount: Account) (bankAccountUSD: Account) (bankAccountEUR: Account) =
+    if bankAccountCHF.Balance >= amount then
+        let newBankAccountCHF = { bankAccountCHF with Balance = bankAccountCHF.Balance - amount }
+        let newCashAccountCHF = { cashAccount with Balance = cashAccount.Balance + amount }
+        (newBankAccountCHF, newCashAccountCHF, bankAccountUSD, bankAccountEUR, Some(sprintf "Withdrawn %M CHF from bank to cash" amount))
+    else
+        (bankAccountCHF, cashAccount, bankAccountUSD, bankAccountEUR, Some("Insufficient funds in bank account"))
+
+let deposit amount (bankAccountCHF: Account) (cashAccountCHF: Account) (bankAccountUSD: Account) (bankAccountEUR: Account) =
+    if cashAccountCHF.Balance >= amount then
+        let newBankAccountCHF = { bankAccountCHF with Balance = bankAccountCHF.Balance + amount }
+        let newCashAccountCHF = { cashAccountCHF with Balance = cashAccountCHF.Balance - amount }
+        (newBankAccountCHF, newCashAccountCHF, bankAccountUSD, bankAccountEUR, Some(sprintf "Deposited %M CHF from cash to bank" amount))
+    else
+        (bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR, Some("Insufficient funds in cash account"))
+
+let showBalances (bankAccountCHF: Account) (cashAccountCHF: Account) (bankAccountUSD: Account) (bankAccountEUR: Account) =
+    let balances = sprintf "Cash Account Balance:\nCHF:     %M\nBank Account Balance:\nCHF:     %M\nUSD:     %M\nEUR:     %M" 
+                            cashAccountCHF.Balance bankAccountCHF.Balance bankAccountUSD.Balance bankAccountEUR.Balance
+    balances
+
+let saveAccounts (bankAccountCHF: Account) (cashAccountCHF: Account) (bankAccountUSD: Account) (bankAccountEUR: Account) =
+    let data = sprintf "%M,%M,%M,%M" cashAccountCHF.Balance bankAccountCHF.Balance bankAccountUSD.Balance bankAccountEUR.Balance
+    File.WriteAllText("accounts.txt", data)
+    prepareResult(1, true)
+    printfn "Accounts saved"
+    prepareResult(2, false)
+
+let loadAccounts () =
+    if File.Exists("accounts.txt") then
+        let data = File.ReadAllText("accounts.txt").Split(',')
+        let cashAccountCHF = { Type = "Cash"; Currency = "CHF"; Balance = decimal data.[0] }
+        let bankAccountCHF = { Type = "Bank"; Currency = "CHF"; Balance = decimal data.[1] }
+        let bankAccountUSD = { Type = "Bank"; Currency = "USD"; Balance = decimal data.[2] }
+        let bankAccountEUR = { Type = "Bank"; Currency = "EUR"; Balance = decimal data.[3] }
+        prepareResult(1, true)
+        printfn "Accounts loaded"
+        bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+    else
+        prepareResult(1, true)
+        printfn "No saved accounts found"
+        bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+
+let tradeExchange (bankAccountFrom: Account) (bankAccountTo: Account) (amount: decimal) =
+    // Import API KEY from file
+    let apiKey = File.ReadAllText("APIKEY.txt").Trim()
+    // Fetch exchange rates
+    let exchangeRatesTask = fetchExchangeRates apiKey |> Async.RunSynchronously
+    let rate = match bankAccountFrom.Currency, bankAccountTo.Currency with
+                        | "CHF", "USD" -> calculateExchangeRate "CHF" "USD" exchangeRatesTask
+                        | "USD", "CHF" -> calculateExchangeRate "USD" "CHF" exchangeRatesTask
+                        | "CHF", "EUR" -> calculateExchangeRate "CHF" "EUR" exchangeRatesTask
+                        | "EUR", "CHF" -> calculateExchangeRate "EUR" "CHF" exchangeRatesTask
+                        | "USD", "EUR" -> calculateExchangeRate "USD" "EUR" exchangeRatesTask
+                        | "EUR", "USD" -> calculateExchangeRate "EUR" "USD" exchangeRatesTask
+                        | _, _ -> 0M
+    if bankAccountFrom.Balance >= amount then
+        let newBankAccountFrom = { bankAccountFrom with Balance = Math.Round(bankAccountFrom.Balance - amount,2) }
+        let newBankAccountTo = { bankAccountTo with Balance = Math.Round(bankAccountTo.Balance + amount * rate,2) }
+        newBankAccountFrom, newBankAccountTo
+    else 
+        prepareResult(1, true)
+        printfn "Insufficient funds in bank account"
+        bankAccountFrom, bankAccountTo
+
+let trade (bankAccountCHF: Account) (cashAccountCHF: Account) (bankAccountUSD: Account) (bankAccountEUR: Account)  =
+    let apiKey = File.ReadAllText("APIKEY.txt").Trim()
+    printfn "Trading is only available in your bank account"
+    printfn "Your Bank Accounts:"
+    printfn "CHF     %M" bankAccountCHF.Balance
+    printfn "USD     %M" bankAccountUSD.Balance
+    printfn "EUR     %M" bankAccountEUR.Balance
+    prepareResult(1, false)
+    let tradeableFrom = [ "CHF"; "USD"; "EUR" ]
+    printf "Select currency to trade from (CHF, USD, EUR): "
+    let tradeFrom = Console.ReadLine()
+    let tradeableTo = tradeableFrom |> List.filter(fun x -> x <> tradeFrom)
+    printf "Select currency to trade to (%s, %s): " tradeableTo.[0] tradeableTo.[1]
+    let tradeTo = Console.ReadLine()
+    printf "Amount to trade: "
+    let amount = decimal (Console.ReadLine())
+    prepareResult(1,true)
+    let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR =
+        match tradeFrom, tradeTo with
+        | "CHF", "USD" ->
+            let newBankAccountCHF, newBankAccountUSD = tradeExchange bankAccountCHF bankAccountUSD amount
+            printfn "Trading %M CHF to USD" amount
+            newBankAccountCHF, cashAccountCHF, newBankAccountUSD, bankAccountEUR
+        | "USD", "CHF" ->
+            let newBankAccountUSD, newBankAccountCHF = tradeExchange bankAccountUSD bankAccountCHF amount
+            printfn "Trading %M USD to CHF" amount
+            newBankAccountCHF, cashAccountCHF, newBankAccountUSD, bankAccountEUR
+        | "CHF", "EUR" ->
+            let newBankAccountCHF, newBankAccountEUR = tradeExchange bankAccountCHF bankAccountEUR amount
+            printfn "Trading %M CHF to EUR" amount
+            newBankAccountCHF, cashAccountCHF, bankAccountUSD, newBankAccountEUR
+        | "EUR", "CHF" ->
+            let newBankAccountEUR, newBankAccountCHF = tradeExchange bankAccountEUR bankAccountCHF amount
+            printfn "Trading %M EUR to CHF" amount
+            newBankAccountCHF, cashAccountCHF, bankAccountUSD, newBankAccountEUR
+        | "USD", "EUR" ->
+            let newBankAccountUSD, newBankAccountEUR = tradeExchange bankAccountUSD bankAccountEUR amount
+            printfn "Trading %M USD to EUR" amount
+            bankAccountCHF, cashAccountCHF, newBankAccountUSD, newBankAccountEUR
+        | "EUR", "USD" ->
+            let newBankAccountEUR, newBankAccountUSD = tradeExchange bankAccountEUR bankAccountUSD amount
+            printfn "Trading %M EUR to USD" amount
+            bankAccountCHF, cashAccountCHF, newBankAccountUSD, newBankAccountEUR
+        | _ ->
+            prepareResult(1, true)
+            printfn "Invalid trade pair"
+            bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+    newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR
+
+let coinToss (bankAccountCHF: Account) (cashAccountCHF: Account) (bankAccountUSD: Account) (bankAccountEUR: Account)  =
+    printfn "Coin Toss is only available in your cash account"
+    printfn "Your Cash Account Balance:"
+    printfn "CHF     %M" cashAccountCHF.Balance
+    prepareResult(1, false)
+    printfn "Enter amount to bet: "
+    let amount = decimal (Console.ReadLine())
+    if amount > cashAccountCHF.Balance then
+        prepareResult(1, true)
+        printfn "Insufficient funds in cash account"
+        bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+    else
+        printfn "Heads or Tails: "
+        let decision = Console.ReadLine().Trim().ToLower()
+        let coin = if Random().Next(0, 2) = 0 then "heads" else "tails"
+        let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR =
+            if decision = coin then
+                let newCashAccountCHF = { cashAccountCHF with Balance = cashAccountCHF.Balance + amount }
+                printfn "You won %M CHF" amount
+                bankAccountCHF, newCashAccountCHF, bankAccountUSD, bankAccountEUR
+            else
+                let newCashAccountCHF = { cashAccountCHF with Balance = cashAccountCHF.Balance - amount }
+                printfn "You lost %M CHF" amount
+                bankAccountCHF, newCashAccountCHF, bankAccountUSD, bankAccountEUR
+        bankAccountCHF, newCashAccountCHF, bankAccountUSD, bankAccountEUR
+
+let main () =
+    let bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR = loadAccounts()
+    let rec loop (bankAccountCHF: Account) (cashAccountCHF: Account) (bankAccountUSD: Account) (bankAccountEUR: Account)=
+        prepareResult(2, false)
+        printf "1. Withdraw\n2. Deposit\n3. Currency Exchange\n4. Coin Toss\n5. Show Balances\n6. Save and Exit\n> "
+        let input = Console.ReadLine()
+        let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR =
+            match input with
+            | "1" -> 
+                printf "Amount to withdraw: "
+                let amount = decimal (Console.ReadLine())
+                prepareResult(1,true)
+                let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR, message = 
+                    withdraw amount bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+                printfn "%s" (message |> Option.defaultValue "")
+                newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR
+            | "2" -> 
+                printf "Amount to deposit: "
+                let amount = decimal (Console.ReadLine())
+                prepareResult(1,true)
+                let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR, message = 
+                    deposit amount bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+                printfn "%s" (message |> Option.defaultValue "")
+                newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR
+            | "3" -> 
+                prepareResult(1, true)
+                let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR = 
+                    trade bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+                newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR
+            | "4" -> 
+                prepareResult(1, true)
+                let newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR = 
+                    coinToss bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+                newBankAccountCHF, newCashAccountCHF, newBankAccountUSD, newBankAccountEUR
+            | "5" -> 
+                let balances = showBalances bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+                prepareResult(1,true)
+                printfn "%s" balances
+                bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+            | "6" -> 
+                prepareResult(1,true)
+                saveAccounts bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+                bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+            | _ -> 
+                prepareResult(1, true)
+                printfn "Invalid option"
+                bankAccountCHF, cashAccountCHF, bankAccountUSD, bankAccountEUR
+            
+        if input <> "6" then 
+            loop newBankAccountCHF newCashAccountCHF newBankAccountUSD newBankAccountEUR
+    loop bankAccountCHF cashAccountCHF bankAccountUSD bankAccountEUR
+
+main()
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # FinancialGame
+## Motivation
+This Project is part of my seminar *Programming Languages and Paradigms*. In this course I decided on learning more about F# since it enhances my understanding of functional programming and was a language that i wasn't already familiar with.
+
+## Usage
+To start the program enter "dotnet run" while you are in the main folder.
+
+As soon as the program runs you can see what you are able to do in the console.
+- Withdrawing is only possible in your "CHF" Accounts.
+- Deposit is only possible in your "CHF" Accounts.
+- Currency exchange is only possible between bank accounts. It uses a API to get the current exchange rates.
+- Coin Toss is a "Casino Game". This is only available with your Cash account.
+- Showing the Balances is used to see how your current account balances are.
+- Saving and exiting the program stores your current accounts to a file so you can continue at the same point later on.
+
+## API Used
+For the submission I decided leaving my API key in the program so you can test my solution without having to create your own API key.
+
+How you would add your own API key:
+
+Create an account for the following API.
+
+You get 5000 API calls per month for free.
+
+*https://app.freecurrencyapi.com/dashboard*
+
+Enter your API key with the full URL in to a "APIKEY.txt" file as follows:
+
+*https://api.freecurrencyapi.com/v1/latest?apikey=YOURKEYHERE*
+
+
+## Nice to haves implemented
+# Gambling
+For Gambling I implemented a street gambling game. A Coin Toss for double or nothing. This is only possible with your cash account since its like "casino/street" gambling.
+
+## Unit Testing
+I tried implementing unit tests with xUnit, nUnit and Expecto.
+xUnit and nUnit are those trying to integrate into your IDE's testing suite while Expecto is running purely in the console.
+
+Unfortunately F# has such bad Error handling that even after trying for several days, I wasn't able to fix the problems and had to submit the project without any tests. This was a hard decision to make since you told me you wished for unit tests but at some point I had to start learning for other exams since the first one is already one week after the deadline for this assignment.
+
+# Things i've tried
+First I decided to implement xUnit. This seemed partical since it is a well established testing solution for .NET languages. I first setup a new testing application and tryed to run the tests. Unfortunately I tried to import my F# Project to run the tests but this didn't work as I hoped. I searched on the F# forum, stackoverflow and asked about 10000 questions to chatgpt (not really that many but it felt like waaaay to many prompts...) The solutions I found couldn't help me and after that I decided to try the same thing with nUnit since this also was described as a good choice for F# and I thought maybe I can fix this issue with another testing solution. Well arround 12 hours (in several days) of fidgeting around with those two I thought I would give up. 
+
+Then I had the idea to break the laws of good practice and make the unittest inside my project. Here I also tried both version and it seemed I am getting closer to it actually working. But the issue I had that my normal program didn't work anymore so you could no longer execute it and then some I had some other issues. Even though I finally could import the program I coudln't write executable testcases into the testfile but I couldn't find out why it wasn't executable... I tried asking chatgpt, looking in forums but I didn't find anything..
+
+Last but not least i tryed implementing Expecto. I thought leaving the standard solutions and trying something else may help. But to be honest I was already so frustrated and had to start learning for other courses that as soon as I reached some similar issues that I faced before I already lost the motivation to continue working on it. Finally I decided to give up and hope that my solution was sufficient even without implementing unittests..
+
+In total I spent about 2 weeks (not full days but around 1-2h a day) trying to find solutions that may be useful but none od them have actually worked.
